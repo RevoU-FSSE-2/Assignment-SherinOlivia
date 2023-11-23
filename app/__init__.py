22564242f053e12ac51af8ae6bf32b4ed9785a59
Sherin Olivia <SOChronicle@gmail.com>
2023-11-23 18:36:32 +0700

    chore: registered tweet and follow blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,9 @@ import os
 from flask import Flask
 from app.user.apis import user_blueprint
 from app.auth.apis import auth_blueprint
-# from app.tweet.apis import tweet_blueprint
-from infrastructure.db import db, db_init
+from app.tweet.apis import tweet_blueprint
+from app.follow.apis import follow_blueprint
+from infrastructure.db import db
 from app.common.bcrypt import bcrypt
 from dotenv import load_dotenv
 
@@ -16,9 +17,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 db.init_app(app)
 bcrypt.init_app(app)
 
-# app.register_blueprint(tweet_blueprint, url_prefix="/tweet")
+app.register_blueprint(tweet_blueprint, url_prefix="/tweet")
 app.register_blueprint(auth_blueprint, url_prefix="/auth")
+app.register_blueprint(follow_blueprint, url_prefix="/following")
 app.register_blueprint(user_blueprint)
 
-with app.app_context():
-    db_init()
+# with app.app_context():
+#     db_init()
