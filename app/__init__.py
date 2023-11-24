92695c528909b7f82764f0442ef183b7b5c7b187
Sherin Olivia <SOChronicle@gmail.com>
2023-11-24 19:18:27 +0700

    chore: removed bcrypt import and used hashingmodule

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,7 @@ from app.user.apis import user_blueprint
 from app.auth.apis import auth_blueprint
 from app.tweet.apis import tweet_blueprint
 from app.follow.apis import follow_blueprint
-from infrastructure.db import db
-from app.common.bcrypt import bcrypt
+from infrastructure.db import db, db_init
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,12 +14,17 @@ database_url = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 db.init_app(app)
-bcrypt.init_app(app)
 
 app.register_blueprint(tweet_blueprint, url_prefix="/tweet")
 app.register_blueprint(auth_blueprint, url_prefix="/auth")
 app.register_blueprint(follow_blueprint, url_prefix="/following")
-app.register_blueprint(user_blueprint)
+app.register_blueprint(user_blueprint, url_prefix="/user")
+
+@app.route("/", methods=["GET"])
+def hello_world():
+    return {{
+        "message": "Hi! Welcome to Sherin Olivia's W21 Assignment..!"
+    }}, 200
 
 # with app.app_context():
 #     db_init()
--- app/di.py
+++ app/di.py
@@ -2,9 +2,11 @@ from injector import Injector
 from infrastructure.user.modules import UserModule
 from infrastructure.tweet.modules import TweetModule
 from infrastructure.follow.modules import FollowModule
+from infrastructure.auth.modules import HashingModule
 
 injector = Injector([
     UserModule,
     TweetModule,
-    FollowModule
+    FollowModule,
+    HashingModule
 ])
