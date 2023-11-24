e1f4fb308f786df78beb9cfd7cf07d1849160182
Sherin Olivia <SOChronicle@gmail.com>
2023-11-24 19:20:46 +0700

    feat: added new items to response for user's  profile 'followers, following, tweets'

--- app/user/apis.py
+++ app/user/apis.py
@@ -1,15 +1,14 @@
-import os
 from flask import Blueprint, request
 from core.user.services import UserService
-from infrastructure.user.models import User
+from infrastructure.tweet.models import Tweet
+from infrastructure.follow.models import Follow
 from app.auth.utils import decode_jwt
 from app.di import injector
-# from datetime import datetime, timedelta
 
 user_blueprint = Blueprint('user', __name__)
 user_service = injector.get(UserService)
 
-@user_blueprint.route('/user', methods=['GET'])
+@user_blueprint.route('', methods=['GET'])
 def get_user():
     token = request.headers.get('Authorization')
     token_payload = decode_jwt(token)
@@ -25,8 +24,35 @@ def get_user():
     if not user:
         return {"error_message": "User not Found"}, 404
 
+    tweets = Tweet.query.filter_by(user_id=user_id).order_by(Tweet.published_at.desc()).limit(10).all()
+
+    list_of_tweets = []
+    for tweet in tweets:
+        tweet_data = {
+            'id': tweet.id,
+            'tweet': tweet.tweet,
+            'published_at': tweet.published_at
+        }
+        list_of_tweets.append(tweet_data)
+
+    follower_count = Follow.query.filter_by(target_id=user_id, is_following=True).count()
+    if Follow.query.filter_by(target_id=user_id).count() > 0:
+        follower_count = follower_count
+    else:
+        follower_count = 0
+
+    following_count = Follow.query.filter_by(user_id=user_id, is_following=True).count()
+    if Follow.query.filter_by(user_id=user_id).count() > 0:
+        following_count = following_count
+    else:
+        following_count = 0
+
     return {
         'user_id': user.id,
         'username': user.username,
-        'bio': user.bio
+        'email': user.email,
+        'bio': user.bio,
+        'follower': follower_count,
+        'following': following_count,
+        'tweets': list_of_tweets
     }, 200
